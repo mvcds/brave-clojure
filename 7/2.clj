c5e978e1cc2c95ac3ba495d9a9adace4b4d8c370
Marcos Vinícius C. da Silva <marcos.silva@chamatheapp.com>
2019-02-01 22:37:14 -0200

    use another operator

--- 7/2.clj
+++ 7/2.clj
@@ -15,10 +15,10 @@
         (first infixed)
         (nth infixed 2)))
 
-(macroexpand '(infix (1 + 3 * 4 -5)))
+(macroexpand '(infix (1 + 3 * 4 - 5)))
 
-; (def control (+ (* 3 4) 1 -5))
-; (def result (infix (1 + 3 * 4 -5)))
+; (def control (- (+ 1 (* 3 4) 5)))
+; (def result (infix (1 + 3 * 4 - 5)))
 
 ; (* 3 4) => 1 + (3 * 4) -5 => 
 ; (+ 1 12 -5) => (+ 1 12 -5)
