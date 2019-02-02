9d7a1bd6cbb32e68a7e5c0850599f72ca48c5a22
Marcos Vinícius C. da Silva <marcos.silva@chamatheapp.com>
2019-02-02 19:28:26 -0200

    WIP solves infix through a tree

--- 7/2.clj
+++ 7/2.clj
@@ -9,6 +9,18 @@
         (first infixed)
         (last infixed)))
  
+(defn does-root-have-bigger-precedent?
+  [rootNode leafNode]
+    (let [a (.indexOf precedence rootNode)
+          b (.indexOf precedence leafNode)]
+       (< a b)))
+
+(defn is-root-bigger?
+  [rootNode leafNode]
+    (cond
+       (and (function? rootNode) (function? leafNode)) (does-root-have-bigger-precedent? rootNode leafNode)
+       :else (function? leafNode)))
+
 (defmacro infix
   [infixed]
   (list (second infixed)
@@ -16,6 +28,17 @@
         (nth infixed 2)))
 
 (macroexpand '(infix (1 + 3 * 4 - 5)))
+
+;            1
+;                   +
+;               3                *
+;                 4
+;                       5            -
+
+ ; [- 5 * 4 3 + 1]
+ ; [- 5 (* 4 3) + 1]
+ ; [- 5 (+ (* 4 3) 1)]
+ ; [(- 5 (+ (* 4 3) 1)]
 
 ; (def control (- (+ 1 (* 3 4) 5)))
 ; (def result (infix (1 + 3 * 4 - 5)))
